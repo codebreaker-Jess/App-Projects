142413505a5703c85a64be5234e01f164d09e8bc
codebreaker-Jess <48462139+codebreaker-Jess@users.noreply.github.com>
2020-04-05 19:23:28 -0700

    commit

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,2 +1,25 @@
+import 'package:flame/components/component.dart';
+import 'package:flame/components/mixins/tapable.dart';
+import 'package:flutter/cupertino.dart';
 
+class TapableComponent extends PositionComponent with Tapable {
 
+  // update and render omitted
+
+  @override
+  void onTapUp(TapUpDetails details) {
+    print("tap up");
+  }
+
+  @override
+  void onTapDown(TapDownDetails details) {
+    print("tap down");
+  }
+
+  @override
+  void onTapCancel() {
+    print("tap cancel");
+  }
+
+  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+}
