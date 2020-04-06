56cce6d9fc6efc2b07295aa8a09c59aee2e56f04
codebreaker-Jess <48462139+codebreaker-Jess@users.noreply.github.com>
2020-04-06 16:53:25 -0700

    commit

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -22,97 +23,117 @@ class TimerAppState extends State<TimerApp> {
     }
   }
 
+          
   @override
-  Widget build(BuildContext context) {
-    MaterialApp();
-    if (timer == null)
+    Widget build(BuildContext context) {
+      MaterialApp();
+      if (timer == null)
       timer = Timer.periodic(duration, (Timer t) {
-        handleTick();
+         handleTick();
       });
-
-    int seconds = secondsPassed % 60;
-    int minutes = secondsPassed ~/ 60;
-    int hours = secondsPassed ~/ (60 * 60);
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Welcome to Flutter',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text('Timer'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CustomTextContainer(
-                      label: 'HRS', value: hours.toString().padLeft(2, '0')),
-                  CustomTextContainer(
-                      label: 'MIN', value: minutes.toString().padLeft(2, '0')),
-                  CustomTextContainer(
-                      label: 'SEC', value: seconds.toString().padLeft(2, '0')),
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: RaisedButton(
-                  child: Text(isActive ? 'STOP' : 'START'),
-                  onPressed: () {
-                    setState(() {
-                      isActive = !isActive;
-                    });
-                  },
+          
+              int seconds = secondsPassed % 60;
+              int minutes = secondsPassed ~/ 60;
+              int hours = secondsPassed ~/ (60 * 60);
+          
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Welcome to Flutter',
+                theme: ThemeData(
+                  primarySwatch: Colors.red,
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+                home: Scaffold(
+                  backgroundColor: Colors.blueGrey,
+                  appBar: AppBar(
+                    title: Text('Timer'),
+                  ),
+                  body: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CustomTextContainer(
+                                label: 'HRS', value: hours.toString().padLeft(2, '0')),
+                            CustomTextContainer(
+                                label: 'MIN', value: minutes.toString().padLeft(2, '0')),
+                            CustomTextContainer(
+                                label: 'SEC', value: seconds.toString().padLeft(2, '0')),
+                          ],
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: RaisedButton(
+                            child: Text(isActive ? 'STOP' : 'START'),
+                            onPressed: () {
+                              setState(() {
+                                isActive = !isActive;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
+          }
+          
+          class CustomTextContainer extends StatelessWidget {
+            CustomTextContainer({this.label, this.value});
+          
+            final String label;
+            final String value;
+          
+            @override
+            Widget build(BuildContext context) {
+              return Container(
+                margin: EdgeInsets.symmetric(horizontal: 5),
+                padding: EdgeInsets.all(20),
+                decoration: new BoxDecoration(
+                  borderRadius: new BorderRadius.circular(10),
+                  color: Colors.black,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      '$value',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '$label',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }
+          }
 
-class CustomTextContainer extends StatelessWidget {
-  CustomTextContainer({this.label, this.value});
-
-  final String label;
-  final String value;
-
-  @override
+ @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 5),
-      padding: EdgeInsets.all(20),
-      decoration: new BoxDecoration(
-        borderRadius: new BorderRadius.circular(10),
-        color: Colors.black,
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text(
-            '$value',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 60,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            '$label',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          )
-        ],
-      ),
+    return Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.red,
+          items: <Widget>[
+            Icon(Icons.add, size: 30),
+            Icon(Icons.list, size: 30),
+            Icon(Icons.compare_arrows, size: 30),
+            Icon(Icons.call_split, size: 30),
+            Icon(Icons.perm_identity, size: 30),
+          ],
+          onTap: (index) {
+          },
+        ),
+        body: Container(color: Colors.red),
     );
   }
-}
